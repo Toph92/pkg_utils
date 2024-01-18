80d3f83c9cf81813142f45a9db435fb4381e7be6
Christophe Desbois <cdesbois@free.fr>
2024-01-18 18:03:40 +0100

    sdfsdfs

--- lib/pkg_utils.dart
+++ lib/pkg_utils.dart
@@ -9,8 +9,8 @@ void printColor(dynamic color, String? message,
     [dynamic color2, String? message2, dynamic color3, String? message3]) {
   assert(color is PrintColor || color is int);
   if (color is PrintColor) color = color.value;
-  if (color2 is PrintColor) color2 = color.value;
-  if (color3 is PrintColor) color3 = color.value;
+  if (color2 is PrintColor) color2 = color2.value;
+  if (color3 is PrintColor) color3 = color2.value;
 
   if (color3 != null &&
       message3 != null &&
